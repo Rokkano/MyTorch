6a2a2c4f395eb08f80962a816122562673ed4127
Rokka <pdelaruffie@gmail.com>
2025-06-05 10:42:02 +0200

    Removed syntax errors (CHECK BEFORE COMMIT)

--- src/tensor/tensor_math.hxx
+++ src/tensor/tensor_math.hxx
@@ -4,9 +4,9 @@ template <typename T>
 Tensor<T> Tensor<T>::min()
 {
     T min = this->buffer_[0];
-    for (std::size_t i = 0; i < this->.numel(); i++)
-        if (this->.buffer_[i] < min)
-            min = this->.buffer_[i];
+    for (std::size_t i = 0; i < this->numel(); i++)
+        if (this->buffer_[i] < min)
+            min = this->buffer_[i];
     return Tensor<T>({1}, {min});
 }
 
@@ -14,9 +14,9 @@ template <typename T>
 Tensor<T> Tensor<T>::min(const T &val)
 {
     Tensor<T> tensor = Tensor<T>(this->shape_);
-    for (std::size_t i = 0; i < this->.numel(); i++)
+    for (std::size_t i = 0; i < this->numel(); i++)
         tensor.buffer_[i] = val < this->buffer_[i] ? val : this->buffer_[i];
-    return tensor
+    return tensor;
 }
 
 template <typename T>
@@ -26,18 +26,18 @@ Tensor<T> Tensor<T>::min(const Tensor<T> &other)
         throw TensorInvalidShapeException(std::format("Shape {} and {} are invalid for comparison.", this->tensorShapeToStr(this->shape_), other.tensorShapeToStr(other.shape_)));
 
     Tensor<T> tensor = Tensor<T>(this->shape_);
-    for (std::size_t i = 0; i < this->.numel(); i++)
+    for (std::size_t i = 0; i < this->numel(); i++)
         tensor.buffer_[i] = other.buffer_[i] < this->buffer_[i] ? other.buffer_[i] : this->buffer_[i];
-    return tensor
+    return tensor;
 }
 
 template <typename T>
 Tensor<T> Tensor<T>::max()
 {
-    T min = this->.buffer_[0];
-    for (std::size_t i = 0; i < this->.numel(); i++)
-        if (this->.buffer_[i] > min)
-            min = this->.buffer_[i];
+    T min = this->buffer_[0];
+    for (std::size_t i = 0; i < this->numel(); i++)
+        if (this->buffer_[i] > min)
+            min = this->buffer_[i];
     return Tensor<T>({1}, {min});
 }
 
@@ -45,9 +45,9 @@ template <typename T>
 Tensor<T> Tensor<T>::max(const T &val)
 {
     Tensor<T> tensor = Tensor<T>(this->shape_);
-    for (std::size_t i = 0; i < this->.numel(); i++)
+    for (std::size_t i = 0; i < this->numel(); i++)
         tensor.buffer_[i] = val > this->buffer_[i] ? val : this->buffer_[i];
-    return tensor
+    return tensor;
 }
 
 template <typename T>
@@ -57,7 +57,7 @@ Tensor<T> Tensor<T>::max(const Tensor<T> &other)
         throw TensorInvalidShapeException(std::format("Shape {} and {} are invalid for comparison.", this->tensorShapeToStr(this->shape_), other.tensorShapeToStr(other.shape_)));
 
     Tensor<T> tensor = Tensor<T>(this->shape_);
-    for (std::size_t i = 0; i < this->.numel(); i++)
+    for (std::size_t i = 0; i < this->numel(); i++)
         tensor.buffer_[i] = other.buffer_[i] > this->buffer_[i] ? other.buffer_[i] : this->buffer_[i];
-    return tensor
+    return tensor;
 }
