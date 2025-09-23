4f62eaa4f7a5496a98e035e0b0b5b4da62c73091
Rokka <pdelaruffie@gmail.com>
2025-09-23 20:34:33 +0200

    Added utils

--- src/utils.hxx
+++ src/utils.hxx
@@ -5,6 +5,7 @@
 #include <string>
 #include <sstream>
 #include <numeric>
+#include <cxxabi.h>
 
 template <typename T>
 std::string reccursiveStr(std::vector<std::size_t> shape, T *buffer)
@@ -17,4 +18,19 @@ std::string reccursiveStr(std::vector<std::size_t> shape, T *buffer)
         str += reccursiveBuild(std::vector<std::size_t>(shape.begin() + 1, shape.end()), buffer + i * step) + (i != shape[0] - 1 ? "," : "");
     str = str + "]";
     return str;
+}
+
+template <typename T>
+std::string type_name()
+{
+    // https://stackoverflow.com/questions/1055452/c-get-name-of-type-in-template
+    int status;
+    std::string tname = typeid(T).name();
+    char *demangled_name = abi::__cxa_demangle(tname.c_str(), NULL, NULL, &status);
+    if (status == 0)
+    {
+        tname = demangled_name;
+        std::free(demangled_name);
+    }
+    return tname;
 }
