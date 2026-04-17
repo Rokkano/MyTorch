85ee1293b97884ce4cee817fc6282134abc8d893
Rokka <pdelaruffie@gmail.com>
2026-04-17 10:21:50 +0200

    Modified CMAKeList and build.sh to handle new Backend modifications

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ Options:
   -c, --clean   Clean CMake files instead of running CMake
   -d, --debug   Run CMake in debug mode
   -s, --subdir  Ask CMake to build files in the build/ folder
+Backends:
+  --eigen 		Use the Eigen BLAS backend
+  --cpp			Use the C++ default backend
 
 EOF
 }
@@ -16,6 +19,8 @@ EOF
 CLEAN=0
 DEBUG=0
 SUBDIR=0
+BACKEND_EIGEN=0
+BACKEND_CPP=0
 
 # Parse options
 while true; do
@@ -23,6 +28,8 @@ while true; do
 		-c|--clean) CLEAN=1 ; shift ;;
 		-d|--debug) DEBUG=1 ; shift ;;
 		-s|--subdir) SUBDIR=1 ; shift ;;
+		--eigen) BACKEND_EIGEN=1 ; shift ;;
+		--cpp) BACKEND_CPP=1 ; shift ;;
         "") break ;;
 		-h|--help) print_help ; exit 0 ;;
 		*) print_help ; exit 1 ;;
@@ -30,6 +37,18 @@ while true; do
 done
 
 # Select build directory for CMake
+if [ $BACKEND_EIGEN -eq 1 ] ; then
+	BACKEND_EIGEN_STR="ON"
+	BACKEND_CPP_STR="OFF"
+elif [ $BACKEND_CPP -eq 1 ] ; then
+	BACKEND_EIGEN_STR="OFF"
+	BACKEND_CPP_STR="ON"
+else
+	BACKEND_EIGEN_STR="OFF"
+	BACKEND_CPP_STR="ON"
+fi
+
+# Handle Backend parameters
 if [ $SUBDIR -eq 1 ] ; then
 	BUILD_DIRECTORY="./build"
 else
@@ -57,7 +76,7 @@ else
 	fi
 
 	# Compile with CMake
-	cmake -B $BUILD_DIRECTORY -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" .
+	cmake -B $BUILD_DIRECTORY -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBACKEND_EIGEN=$BACKEND_EIGEN -DBACKEND_CPP=$BACKEND_CPP .
 	if [ $? -eq 0 ] && [ $BUILD_DIRECTORY != "." ]; then
 		echo "(i) Please 'cd $BUILD_DIRECTORY' to make and run the program"
 	fi
