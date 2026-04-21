5125c58a3c67cd339e50f8a84ebd52d36851daa4
Rokka <pdelaruffie@gmail.com>
2026-04-21 10:33:35 +0200

    Updated clang tidy to proper function

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -4,8 +4,17 @@ find_program(CLANG_FORMAT "clang-format")
 
 if(CLANG_FORMAT)
     message(STATUS "${BoldWhite}Adding clang-format target to Makefile${ColorReset}")
-    file(GLOB_RECURSE ALL_CXX_SOURCE_FILES *.cpp *.cxx *.cc *.hh *.hxx)
-    add_custom_target(clang-format COMMAND /usr/bin/clang-format -i -style=file ${ALL_CXX_SOURCE_FILES})
+
+    file(GLOB_RECURSE CLANG_FORMAT_TARGETS CONFIGURE_DEPENDS 
+        "${PROJECT_SOURCE_DIR}/src/*.cc" 
+        "${PROJECT_SOURCE_DIR}/src/*.hh" 
+        "${PROJECT_SOURCE_DIR}/src/*.hxx" 
+        "${PROJECT_SOURCE_DIR}/tests/*.cc" 
+        "${PROJECT_SOURCE_DIR}/tests/*.hh" 
+        "${PROJECT_SOURCE_DIR}/tests/*.hxx" 
+    )
+    add_custom_target(clang-format COMMAND /usr/bin/clang-format -i -style=file ${CLANG_FORMAT_TARGETS})
+
 else ()
     message(STATUS "${BoldYellow}clang-format not found${ColorReset}")
 endif()
--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -4,8 +4,17 @@ find_program(CLANG_TIDY "clang-tidy")
 
 if(CLANG_TIDY)
     message(STATUS "${BoldWhite}Adding clang-tidy target to Makefile${ColorReset}")
-    file(GLOB_RECURSE ALL_CXX_SOURCE_FILES *.cpp *.cxx *.cc *.hh *.hxx)
-    add_custom_target(clang-tidy COMMAND /usr/bin/clang-tidy ${ALL_CXX_SOURCE_FILES} -config='' -- -std=c++20 ${INCLUDE_DIRECTORIES})
+
+    file(GLOB_RECURSE CLANG_TIDY_TARGETS CONFIGURE_DEPENDS 
+        "${PROJECT_SOURCE_DIR}/src/*.cc" 
+        "${PROJECT_SOURCE_DIR}/src/*.hh" 
+        "${PROJECT_SOURCE_DIR}/src/*.hxx" 
+        # "${PROJECT_SOURCE_DIR}/tests/*.cc" 
+        # "${PROJECT_SOURCE_DIR}/tests/*.hh" 
+        # "${PROJECT_SOURCE_DIR}/tests/*.hxx" 
+    )
+    add_custom_target(clang-tidy COMMAND /usr/bin/clang-tidy ${CLANG_TIDY_TARGETS} --fix -checks=-*,clang-analyzer-*,-clang-analyzer-cplusplus* -- -std=c++20 -Iinclude -I./)
+
 else ()
     message(STATUS "${BoldYellow}clang-tidy not found${ColorReset}")
 endif()
